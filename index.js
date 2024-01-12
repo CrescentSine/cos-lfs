6c23fbb61deab4a567a3201ee2e064330a09be21
Crescent Moon <4070647+CrescentSine@users.noreply.github.com>
2024-01-13 06:09:42 +0800

    return 404 when batch with unknown operation

--- index.js
+++ index.js
@@ -21,59 +21,58 @@ server.post("/objects/batch", (req, resp) => {
 
     const expires = config.get("expires");
 
+    resp.header("Content-Type", "application/vnd.git-lfs+json");
+
     switch (req.body.operation) {
         case "upload":
-            respData.objects = req.body.objects.map(({oid, size}) => {
-                return {
-                    oid,
-                    size,
-                    authenticated: true,
-                    actions: {
-                        upload: {
-                            href: cos.getObjectUrl({
-                                Bucket: config.get("cosBucket"),
-                                Region: config.get("cosRegion"),
-                                Key: oid,
-                                Method: 'PUT',
-                                Sign: true,
-                                Expires: expires,
-                            }),
-                            header: {
-                                'Content-Type': 'application/octet-stream',
-                            },
-                            expires_in: expires,
-                        }
-                    },
-                };
-            });
+            respData.objects = req.body.objects.map(({ oid, size }) => ({
+                oid,
+                size,
+                authenticated: true,
+                actions: {
+                    upload: {
+                        href: cos.getObjectUrl({
+                            Bucket: config.get("cosBucket"),
+                            Region: config.get("cosRegion"),
+                            Key: oid,
+                            Method: 'PUT',
+                            Sign: true,
+                            Expires: expires,
+                        }),
+                        header: {
+                            'Content-Type': 'application/octet-stream',
+                        },
+                        expires_in: expires,
+                    }
+                },
+            }));
             break;
         case "download":
-            respData.objects = req.body.objects.map(({oid, size}) => {
-                return {
-                    oid,
-                    size,
-                    authenticated: true,
-                    actions: {
-                        download: {
-                            href: cos.getObjectUrl({
-                                Bucket: config.get("cosBucket"),
-                                Region: config.get("cosRegion"),
-                                Key: oid,
-                                Sign: true,
-                                Expires: expires,
-                            }),
-                            expires_in: expires,
-                        }
-                    },
-                };
-            });
+            respData.objects = req.body.objects.map(({ oid, size }) => ({
+                oid,
+                size,
+                authenticated: true,
+                actions: {
+                    download: {
+                        href: cos.getObjectUrl({
+                            Bucket: config.get("cosBucket"),
+                            Region: config.get("cosRegion"),
+                            Key: oid,
+                            Sign: true,
+                            Expires: expires,
+                        }),
+                        expires_in: expires,
+                    }
+                },
+            }));
             break;
         default:
-            console.warn("unknown operation:", req.body.operation);
-            break;
+            resp.code(404).send({
+                "message": "Not found",
+                "documentation_url": "https://lfs-server.com/docs/errors",
+            });
+            return;
     }
-
-    resp.header("Content-Type", "application/vnd.git-lfs+json");
     resp.code(200).send(respData);
 });
 
